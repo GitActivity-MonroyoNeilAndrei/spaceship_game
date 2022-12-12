39bfd011a2fbff76db727c2de3eedf2770e3fa92
NeilAndrei <andreimonroyo0@gmail.com>
2022-12-12 15:46:50 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace smooth_moving_picturebox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.moveTimer = new System.Windows.Forms.Timer(this.components);
+            this.rightTxtScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.leftTxtScore = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.gameWinner = new System.Windows.Forms.Label();
+            this.gameWinner2 = new System.Windows.Forms.Label();
+            this.restart = new System.Windows.Forms.Label();
+            this.restart1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // moveTimer
+            // 
+            this.moveTimer.Enabled = true;
+            this.moveTimer.Interval = 2;
+            this.moveTimer.Tick += new System.EventHandler(this.moveTimerEvent);
+            // 
+            // rightTxtScore
+            // 
+            this.rightTxtScore.BackColor = System.Drawing.Color.Transparent;
+            this.rightTxtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightTxtScore.Location = new System.Drawing.Point(661, 9);
+            this.rightTxtScore.Name = "rightTxtScore";
+            this.rightTxtScore.Size = new System.Drawing.Size(127, 49);
+            this.rightTxtScore.TabIndex = 1;
+            this.rightTxtScore.Text = "0";
+            this.rightTxtScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.rightTxtScore.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.MainGameTimerEvent);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.AccessibleRole = System.Windows.Forms.AccessibleRole.Window;
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = global::smooth_moving_picturebox.Properties.Resources.bullet_left;
+            this.pictureBox4.Location = new System.Drawing.Point(-100, 221);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::smooth_moving_picturebox.Properties.Resources.bullet_right;
+            this.pictureBox3.Location = new System.Drawing.Point(-100, 171);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::smooth_moving_picturebox.Properties.Resources.spaceship_left;
+            this.pictureBox2.Location = new System.Drawing.Point(19, 374);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::smooth_moving_picturebox.Properties.Resources.spaceship_right;
+            this.pictureBox1.Location = new System.Drawing.Point(539, 295);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // leftTxtScore
+            // 
+            this.leftTxtScore.BackColor = System.Drawing.Color.Transparent;
+            this.leftTxtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leftTxtScore.Location = new System.Drawing.Point(31, 9);
+            this.leftTxtScore.Name = "leftTxtScore";
+            this.leftTxtScore.Size = new System.Drawing.Size(123, 49);
+            this.leftTxtScore.TabIndex = 4;
+            this.leftTxtScore.Text = "0";
+            this.leftTxtScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Black;
+            this.pictureBox5.Location = new System.Drawing.Point(331, -5);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(10, 461);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Black;
+            this.pictureBox6.Location = new System.Drawing.Point(456, -9);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(10, 461);
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            // 
+            // gameWinner
+            // 
+            this.gameWinner.BackColor = System.Drawing.Color.Transparent;
+            this.gameWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameWinner.Location = new System.Drawing.Point(12, 73);
+            this.gameWinner.Name = "gameWinner";
+            this.gameWinner.Size = new System.Drawing.Size(313, 238);
+            this.gameWinner.TabIndex = 7;
+            // 
+            // gameWinner2
+            // 
+            this.gameWinner2.BackColor = System.Drawing.Color.Transparent;
+            this.gameWinner2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameWinner2.Location = new System.Drawing.Point(494, 73);
+            this.gameWinner2.Name = "gameWinner2";
+            this.gameWinner2.Size = new System.Drawing.Size(285, 198);
+            this.gameWinner2.TabIndex = 8;
+            // 
+            // restart
+            // 
+            this.restart.AutoSize = true;
+            this.restart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restart.Location = new System.Drawing.Point(513, 240);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(0, 31);
+            this.restart.TabIndex = 9;
+            // 
+            // restart1
+            // 
+            this.restart1.AutoSize = true;
+            this.restart1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restart1.Location = new System.Drawing.Point(41, 240);
+            this.restart1.Name = "restart1";
+            this.restart1.Size = new System.Drawing.Size(0, 31);
+            this.restart1.TabIndex = 10;
+            this.restart1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.restart1);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.gameWinner2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.gameWinner);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.leftTxtScore);
+            this.Controls.Add(this.rightTxtScore);
+            this.Name = "Form1";
+            this.Text = "wd";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer moveTimer;
+        private System.Windows.Forms.Label rightTxtScore;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label leftTxtScore;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label gameWinner;
+        private System.Windows.Forms.Label gameWinner2;
+        private System.Windows.Forms.Label restart;
+        private System.Windows.Forms.Label restart1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace smooth_moving_picturebox
+{
+    public partial class Form1 : Form
+    {
+        int leftScore;
+        int rightScore;
+        bool moveRight, moveLeft, moveUp, moveDown;
+        bool moveRight2, moveLeft2, moveUp2, moveDown2;
+        
+        int speed = 7;
+
+        int bulletSpeed = 14;
+
+        bool rightBullet = false;
+        bool leftBullet = false;
+
+        bool rightWinner = false;
+        bool leftWinner = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            resetGame();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void moveTimerEvent(object sender, EventArgs e)
+        {
+            // This is the right player
+            if (moveLeft && pictureBox1.Left > 475)
+            {
+                pictureBox1.Left -= speed;
+            }
+            if (moveRight && pictureBox1.Left < 730)
+            {
+                pictureBox1.Left += speed;
+            }
+            if (moveUp && pictureBox1.Top > 0)
+            {
+                pictureBox1.Top -= speed;
+            }
+            if (moveDown && pictureBox1.Top < 382)
+            {
+                pictureBox1.Top += speed;
+            }
+
+            // This is the left player
+            if (moveLeft2 && pictureBox2.Left > 1)
+            {
+                pictureBox2.Left -= speed;
+            }
+            if (moveRight2 && pictureBox2.Left < 257)
+            {
+                pictureBox2.Left += speed;
+            }
+            if (moveUp2 && pictureBox2.Top > 0)
+            {
+                pictureBox2.Top -= speed;
+            }
+            if (moveDown2 && pictureBox2.Top < 382)
+            {
+                pictureBox2.Top += speed;
+            }
+            // Score of Player Left
+            leftTxtScore.Text = leftScore.ToString();
+            // Socre of Player Right
+            rightTxtScore.Text = rightScore.ToString();
+
+            // leftBullet fire
+            if (leftBullet == true)
+            {
+                pictureBox4.Left += bulletSpeed;
+                if (pictureBox4.Left >= 768)
+                {
+                    leftBullet = false;
+                    pictureBox4.Left = -100;
+                }
+            }
+            // rightBullet fire
+            if (rightBullet == true)
+            {
+                pictureBox3.Left -= bulletSpeed;
+                if (pictureBox3.Left <= 0)
+                {
+                    rightBullet = false;
+                    pictureBox3.Left = -100;
+                }
+            }
+            //right
+            if (pictureBox3.Bounds.IntersectsWith(pictureBox2.Bounds))
+            {
+                rightScore += 1;
+                rightBullet = false;
+                pictureBox3.Left = -100;
+            }
+            // left
+            if (pictureBox4.Bounds.IntersectsWith(pictureBox1.Bounds))
+            {
+                leftScore += 1;
+                leftBullet = false;
+                pictureBox4.Left = -100;
+            }
+            // player left Wins
+            if (leftScore > 20)
+            {
+                leftWinner = true;
+                moveTimer.Stop();
+                gameOver();           
+            }
+            // player right wins
+            if (rightScore > 20)
+            {
+                rightWinner = true;
+                moveTimer.Stop();
+                gameOver();
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            // Player right
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                moveUp = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                moveDown = true;
+            }
+
+            // Player left
+            if (e.KeyCode == Keys.A)
+            {
+                moveLeft2 = true;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                moveRight2 = true;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                moveUp2 = true;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                moveDown2 = true;
+            }
+
+            // Bullet Left
+            if (e.KeyCode == Keys.Space)
+            {
+                if (leftBullet == false)
+                {
+                    pictureBox4 .Top = pictureBox2.Top + 15;
+                    pictureBox4 .Left = pictureBox2.Left + 20;
+                    leftBullet = true;
+                }
+            }
+            // Bullet right
+            if (e.KeyCode == Keys.L)
+            {
+                if (rightBullet == false)
+                {
+                    pictureBox3.Top = pictureBox1.Top + 15;
+                    pictureBox3.Left = pictureBox1.Left + 20;
+                    rightBullet = true;
+                }
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            // Player right
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                moveUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                moveDown = false;
+            }
+
+            // Player left
+            if (e.KeyCode == Keys.A)
+            {
+                moveLeft2 = false;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                moveRight2 = false;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                moveUp2 = false;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                moveDown2 = false;
+            }
+            // game resets
+            if (e.KeyCode == Keys.R && (leftScore >= 20 || rightScore >= 20))
+            {
+                resetGame();
+            }
+        }
+
+        private void resetGame()
+        {
+            moveTimer.Start();
+            leftScore = 0;
+            rightScore = 0;
+            gameWinner.Text = "";
+            gameWinner2.Text = "";
+            restart.Text = "";
+            restart1.Text = "";
+            rightBullet = false;
+            leftBullet = false;
+            pictureBox3.Left = 1000;
+            pictureBox4.Left = 1000;
+        }
+
+        private void gameOver()
+        {
+            // player lett wins
+            if (leftWinner)
+            {
+                gameWinner.Text += "PLAYER LEFT WINS!!!";
+                restart1.Text += "press R to restart";
+            }
+            // player right wins
+            else if (rightWinner)
+            {
+                gameWinner2.Text += "PLAYER RIGHT WINS!!!";
+                restart.Text += "press R to restart";
+            }
+        }
+    }
+}
